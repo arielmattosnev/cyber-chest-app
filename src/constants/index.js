df51c2bbc0a27d7ccb440e975a7e8f34591d29d4
Ariel Mattos <arielmattosnev@gmail.com>
2023-05-25 18:06:05 -0300

    chore: create index files to make clear importations

---
+++ src/constants/index.js
@@ -0,0 +1,142 @@
+import {
+  people01,
+  people02,
+  people03,
+  linkedin,
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  send,
+  shield,
+  star,
+  github,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Recursos",
+  },
+  {
+    id: "product",
+    title: "Produtos",
+  },
+  {
+    id: "clients",
+    title: "Clientes",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Recompensas",
+    content:
+      "O melhor cartão de crédito oferece algumas combinações tentadoras de promoções e prêmios.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Seguro",
+    content:
+      "Nós temas árduas medidas de segurança, para garantir a proteção da sua informação.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balanço financeiro",
+    content:
+      "Um balanço das finanças do cartão de crédito, poderá lhe salvar de juros abusivos e multas.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "O dinheiro é apenas uma ferramenta. Ele o levará aonde você quiser, mas não o substituirá como motorista.",
+    name: "Jane Doe",
+    title: "FUndador",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "O dinheiro facilita a sua vida. Se você tem sorte de tê-lo, você é um sortudo.",
+    name: "Thomas Doe",
+    title: "Fundador",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Geralmente, são as pessoas que trabalham com dinheiro e tecnologia, expandido ao comércio internacional que são realmente ricas.",
+    name: "John Doe",
+    title: "Fundador",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Usuários Ativos",
+    value: "4500+",
+  },
+  {
+    id: "stats-2",
+    title: "Empresas Parceiras",
+    value: "260+",
+  },
+  {
+    id: "stats-3",
+    title: "Transações",
+    value: "$430M+",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: github,
+    title: "Repósitorio do projeto",
+    link: "https://github.com/arielmattosnev/cyberchest-app",
+  },
+  {
+    id: "social-media-3",
+    icon: github,
+    title: "Meu github",
+    link: "https://github.com/arielmattosnev",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    title: "Meu linkedin",
+    link: "https://www.linkedin.com/in/ariel-mattos-03908022b/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
