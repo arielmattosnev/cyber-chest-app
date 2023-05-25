fffddd128a7b5476931c0ff0472f68e3214902c6
Ariel Mattos <arielmattosnev@gmail.com>
2023-05-25 18:03:58 -0300

    feat: finished client component

---
+++ src/components/Clients.jsx
@@ -0,0 +1,26 @@
+import { clients } from "../constants";
+
+import styles from "../styles/style";
+
+const Clients = () => {
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map(({ id, logo }) => (
+          <div
+            key={id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={logo}
+              alt="partner companies"
+              className="sm:w-[192px] w-[100px] object-contain hover:scale-125 duration-200"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Clients;
