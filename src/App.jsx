3ff14c6e85ae87516c2ba4ee0d2e1a670d5d8a43
Ariel Mattos <arielmattosnev@gmail.com>
2023-05-25 18:01:58 -0300

    feat: finished the app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-function App() {
+import styles from "./styles/style";
+
+import {
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
+
+const App = () => {
   return (
-    <div className="bg-slate-500 text-7xl ">
-      <h1>hello World!</h1>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
